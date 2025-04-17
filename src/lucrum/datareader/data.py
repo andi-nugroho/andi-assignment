be3b1623d5a1edd73d3c59fb114ea2432ec4aa7f
NERD <nugrohoandi2005@gmail.com>
2025-04-17 20:06:44 +0700

    Moving avg prediction implemented.

--- src/lucrum/datareader/data.py
+++ src/lucrum/datareader/data.py
@@ -1,1 +1,32 @@
+"""A script which exposes all the datareaders."""
 
+# Author: Dylan Vassallo <dylan.vassallo.18@um.edu.mt>
+
+###### importing dependencies #############################################
+from lucrum.datareader.binance import BinanceData
+
+###### datareaders ########################################################
+def get_data(source, *args, **kwargs):
+    sources = ["binance"]
+
+    if source not in sources:
+        error = "source=%r is not implemented" % source
+        raise NotImplementedError(error)
+    
+    if source == "binance":
+        return BinanceData(*args, **kwargs).read()
+
+    else:
+        error = "source=%r is not implemented" % source
+        raise NotImplementedError(error)
+
+def get_data_binance(*args, **kwargs):
+    """Gets historical data from binance API.
+    
+    Returns 
+    -------
+    
+    """
+    return BinanceData(*args, **kwargs).read()
+
+###########################################################################
